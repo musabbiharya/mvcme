7bfccfedf023a75a07c3573b645009b1841d7379
satria <satria@192.168.0.10>
2017-11-07 22:36:36 +0700

    add callprocedure shorthand

--- libs/Database.php
+++ libs/Database.php
@@ -1,15 +1,13 @@
 <?php
 
-class Database extends PDO
-{
-    
-    public function __construct($DB_TYPE, $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS)
-    {
-        parent::__construct($DB_TYPE.':host='.$DB_HOST.';dbname='.$DB_NAME, $DB_USER, $DB_PASS);
-        
+class Database extends PDO {
+
+    public function __construct($DB_TYPE, $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS) {
+        parent::__construct($DB_TYPE . ':host=' . $DB_HOST . ';dbname=' . $DB_NAME, $DB_USER, $DB_PASS);
+
         //parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTIONS);
     }
-    
+
     /**
      * select
      * @param string $sql An SQL string
@@ -17,63 +15,60 @@ class Database extends PDO
      * @param constant $fetchMode A PDO Fetch mode
      * @return mixed
      */
-    public function select($sql, $array = array(), $fetchMode = PDO::FETCH_ASSOC)
-    {
+    public function select($sql, $array = array(), $fetchMode = PDO::FETCH_ASSOC) {
         $sth = $this->prepare($sql);
         foreach ($array as $key => $value) {
             $sth->bindValue("$key", $value);
         }
-        
+
         $sth->execute();
         return $sth->fetchAll($fetchMode);
     }
-    
+
     /**
      * insert
      * @param string $table A name of table to insert into
      * @param string $data An associative array
      */
-    public function insert($table, $data)
-    {
+    public function insert($table, $data) {
         ksort($data);
-        
+
         $fieldNames = implode('`, `', array_keys($data));
         $fieldValues = ':' . implode(', :', array_keys($data));
-        
+
         $sth = $this->prepare("INSERT INTO $table (`$fieldNames`) VALUES ($fieldValues)");
-        
+
         foreach ($data as $key => $value) {
             $sth->bindValue(":$key", $value);
         }
-        
+
         $sth->execute();
     }
-    
+
     /**
      * update
      * @param string $table A name of table to insert into
      * @param string $data An associative array
      * @param string $where the WHERE query part
      */
-    public function update($table, $data, $where)
-    {
+    public function update($table, $data, $where) {
         ksort($data);
-        
+
         $fieldDetails = NULL;
-        foreach($data as $key=> $value) {
+        foreach ($data as $key => $value) {
             $fieldDetails .= "`$key`=:$key,";
         }
         $fieldDetails = rtrim($fieldDetails, ',');
-        
+
         $sth = $this->prepare("UPDATE $table SET $fieldDetails WHERE $where");
-        
+
         foreach ($data as $key => $value) {
             $sth->bindValue(":$key", $value);
         }
-        
+
         $sth->execute();
     }
-    
+
     /**
      * delete
      * 
@@ -82,9 +77,34 @@ class Database extends PDO
      * @param integer $limit
      * @return integer Affected Rows
      */
-    public function delete($table, $where, $limit = 1)
-    {
+    public function delete($table, $where, $limit = 1) {
         return $this->exec("DELETE FROM $table WHERE $where LIMIT $limit");
     }
-    
+
+    public function callProcedure($table, $data, $out) {
+        ksort($out);
+        ksort($data);
+        $fieldValues = ':' . implode(', :', array_keys($data));
+        $fieldValuesOut = '@' . implode(', @', $out);
+       
+        $query = "call $table($fieldValues,$fieldValuesOut)";
+        $sth = $this->prepare($query);
+        foreach ($data as $key => $value) {
+            $sth->bindValue(":$key", $value);
+        }
+        $sth->execute();
+        $fieldValuesOutArray = explode(',', $fieldValuesOut);
+        
+        $sql = '';
+        foreach ($out as $key => $value) {
+            $sql [$key] = "$fieldValuesOutArray[$key] as $value";
+        }
+        $sqlr = implode(',', $sql);
+        $q2 = " select $sqlr";
+        $sth2 = $this->query($q2);
+
+        $sth2->execute();
+        return $sth2->fetchAll(PDO::FETCH_ASSOC);
+    }
+
 }
